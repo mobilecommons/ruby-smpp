1eb4fa665bc47476292f02252bc4bfe744b09d6b
Mohamed A Mohamed <mohamed_abdelrahman@icloud.com>
2017-01-12 04:56:41 +0200

    encode ucs2 in ruby-smpp

--- lib/smpp/transceiver.rb
+++ lib/smpp/transceiver.rb
@@ -39,6 +39,7 @@ class Smpp::Transceiver < Smpp::Base
       parts = []
 #       logger.debug "Encoding :- #{Encoding.default_external.inspect}"
       if options[:data_coding] == 8
+        message = message.encode(Encoding::UCS_2BE, :invalid => :replace, :undef => :replace, :replace => '')
         logger.debug "data codeing is 8 horray #{message.inspect}"
         message.chars.to_a.each_slice(Smpp::Transceiver.get_message_part_size(options)) do |part|
           parts << part.join
