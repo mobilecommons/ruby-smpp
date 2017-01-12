89045ed743206197ecd7a4123e06ad238aeeb329
Mohamed A Mohamed <mohamed_abdelrahman@icloud.com>
2017-01-12 17:48:00 +0200

    remove encoding inside ruby-smpp

--- lib/smpp/transceiver.rb
+++ lib/smpp/transceiver.rb
@@ -39,9 +39,8 @@ class Smpp::Transceiver < Smpp::Base
       parts = []
 #       logger.debug "Encoding :- #{Encoding.default_external.inspect}"
       if options[:data_coding] == 8
-        message = message.force_encode(Encoding::UCS_2BE)
         logger.debug "data codeing is 8 horray #{message.inspect}"
-        message.chars.to_a.each_slice(Smpp::Transceiver.get_message_part_size(options)) do |part|
+        message.chars.to_a.each_slice(Smpp::Transceiver.get_message_part_size(options) - 1) do |part|
           parts << part.join
         end
       else
