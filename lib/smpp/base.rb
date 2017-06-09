a1a6ef64a95c62b5658984eb805eedf22a78f4f0
Levan Arabuli <LevanArabuli@users.noreply.github.com>
2017-06-09 17:43:32 +0400

    Remove obsolete logs

--- lib/smpp/base.rb
+++ lib/smpp/base.rb
@@ -96,20 +96,15 @@ module Smpp
     # EventMachine::Connection#receive_data
     def receive_data(data)
       #append data to buffer
-      logger.info "debug_mcommons: raw #{data.inspect}"
       @data << data
       while (@data.length >=4)
         cmd_length = @data[0..3].unpack('N').first
         if(@data.length < cmd_length)
           #not complete packet ... break
-          logger.info "debug_mcommons: not a complete packet ! #{@data}"
           break
         end
-        logger.info "debug_mcommons: before data.slice! ! #{@data}"
         pkt = @data.slice!(0,cmd_length)
-        logger.info "debug_mcommons: after data.slice! ! #{@data}"
         begin
-          logger.info "Got one! #{pkt}"
           # parse incoming PDU
           pdu = read_pdu(pkt)
 
